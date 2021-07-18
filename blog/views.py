a710bc6121d91ad14aba555b00aa53bc168866bf
batrav <engineervibhorbatra@gmail.com>
2021-07-18 23:14:32 +0530

    updated folder name

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def starging_page(request):
+    return HttpResponse('starging_page')
+
+def posts(request):
+    return HttpResponse('posts')
+
+def post_detail(request):
+     return HttpResponse('post_detail')
+
